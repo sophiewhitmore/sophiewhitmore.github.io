4b9724797452e5eb4bf3f9b2d1523434a0bc2c45
stevewhitmore <s25w89@gmail.com>
2020-02-23 19:26:57 -0600

    Initial commit

---
+++ code.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('button');
+const article = document.querySelector('article');
+
+button.addEventListener('click', () => {
+    article.classList.toggle('visible');
+});
+
+
+
+// const button = document.querySelector('button');
+// button.addEventListener('click', () => {
+//     alert('Would you like to go to a different webpage?')
+    
+// {)
